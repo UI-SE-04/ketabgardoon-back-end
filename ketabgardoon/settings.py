381049c0ad3f0d9d12d6bbc21b011f72eecda257
Mahdi <mmahdieh90@gmail.com>
2025-04-10 23:38:40 +0330

    fix: changed defualt database to PostgreSQL

--- ketabgardoon/settings.py
+++ ketabgardoon/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'ketabgardoon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+
+        # Uncomment for encrypt db
+        # "OPTIONS": {
+        #     "service": "my_service",
+        #     "passfile": ".pgpass",
+        # },
     }
 }
 
