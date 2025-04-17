6d8953b5b128ee591f1444ef62eaca86640dfe45
Mahdi <mmahdieh90@gmail.com>
2025-04-17 20:58:04 +0330

    feat: code author model and install 'authors' app in settings - Country model should implement

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField(blank=True, null=True)
+    nationality = models.ForeignKey(Nationality, on_delete=models.SET_NULL, null=True, blank=True)
+    rating = models.FloatField(blank=True, null=True)
+    total_sum_of_ratings = models.IntegerField(blank=True, null=True)
+    total_number_of_ratings = models.IntegerField(blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    author_photo = models.ImageField(upload_to='author_photos/', blank=True, null=True)
+    call_info = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
