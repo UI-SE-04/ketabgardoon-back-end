ab2f44860fef53dd80414339c7012a6d08194af5
Mahdi <mmahdieh90@gmail.com>
2025-04-17 21:02:36 +0330

    feat: countries model implemented

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Nationality(models.Model):
+    country = models.CharField(max_length=255)
+    country_code = models.CharField(max_length=2)
