cf0a96ea8c305f1805bd1b2ac45d6d6b3faa0ffb
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-18 11:36:45 +0330

    feat: api route implemented

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,11 +1,15 @@
 from rest_framework.routers import DefaultRouter
+
 from authors.viewsets import AuthorViewSet
 from countries.viewsets import NationalityViewSet
+from comments.viewsets import CommentViewSet, UserCommentLikeViewSet
 # (register other app viewsets here)
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet, basename='author')
 router.register(r'nationalities', NationalityViewSet, basename='nationality')
+router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'comment-likes', UserCommentLikeViewSet, basename='comment-like')
 # e.g. router.register(r'books', BookViewSet)
 
 urlpatterns = router.urls
