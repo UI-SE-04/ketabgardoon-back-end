f67066e675679de666bd32cf81da74660c24e421
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-18 11:13:56 +0330

    feat: implemented the comment model + IMPORTANT NOTE NOTE: THE USER NOT IMPLEMENTED YET SO THE USER OBJECT USAGE COMMENTED FOR NOW

--- comments/models.py
+++ comments/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+# from users.models import User
+
+class Comment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class UserCommentLike(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
