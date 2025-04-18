b05ce2f4fd55d36811b04940dd11c3ae8b0d7f29
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-18 11:43:41 +0330

    fix: remove old manually returned json views

--- countries/viewsets.py
+++ countries/viewsets.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
-from .models import Nationality
-from .serializers import NationalitySerializer
+from .models import Country
+from .serializers import CountrySerializer
 
 class NationalityViewSet(viewsets.ModelViewSet):
     """
     list, retrieve, create, update, partial_update, destroy
     """
-    queryset = Nationality.objects.all().order_by('country')
-    serializer_class = NationalitySerializer
+    queryset = Country.objects.all().order_by('country')
+    serializer_class = CountrySerializer
     search_fields = ['country', 'country_code']
     ordering_fields = ['country']
