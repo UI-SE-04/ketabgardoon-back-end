b8e468c2fef4163057d64ce406691969a3eadfd7
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-18 10:33:34 +0330

    fix: add authors to admin panel - fix the author representation

--- authors/models.py
+++ authors/models.py
@@ -19,3 +19,6 @@ class Author(models.Model):
     call_info = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
