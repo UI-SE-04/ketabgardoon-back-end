3daf1f95f5b45050e8fccb6cfe3ad8e3084bdceb
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-18 09:21:47 +0330

    feat: register api and version urls into settings

--- ketabgardoon/settings.py
+++ ketabgardoon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import include, path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,8 +44,7 @@ INSTALLED_APPS = [
 
     'books',
     'users',
-    'authors',
-    'countries',
+    path('api/v1/', include('api.urls')),
 ]
 
 MIDDLEWARE = [
