3a78eafcbc286a89c46eb9decd92193e459c7497
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-04-21 21:08:19 +0330

    feat: add fields to Book class

--- books/models.py
+++ books/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
 
+    published_date = models.DateField(null=True, blank=True)
+    cover = models.ImageField(upload_to='books/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    categories = models.ManyToManyField('Category')
+    stores = models.ManyToManyField('Store', through='BookStore')
 
     def __str__(self):
         return self.title
