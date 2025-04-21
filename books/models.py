b52ec15eec7432d025f7499cbeb3001c7cd40478
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-04-21 20:49:40 +0330

    fix: Remove publisher and auther class from books\models.py

--- books/models.py
+++ books/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class Publisher(models.Model):
-    pass
-
-
-class Author(models.Model):
-    pass
 
 class Role(models.Model):
     title = models.CharField(max_length=50)
@@ -18,14 +12,14 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     summary = models.TextField(blank=True)
-    publisher = models.ForeignKey(Publisher, null=True, blank=True, on_delete=models.SET_NULL, related_name='books')
+    publisher = models.ForeignKey( null=True, blank=True, on_delete=models.SET_NULL, related_name='books')
     published_date = models.DateField(null=True, blank=True)
     cover = models.ImageField(null=True, blank=True)  # Consider ImageField for file-based images.
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     # Many-to-many relationship with authors is managed via a through model.
-    authors = models.ManyToManyField(Author, through='BookAuthor', related_name='books')
+    authors = models.ManyToManyField( through='BookAuthor', related_name='books')
 
     def __str__(self):
         return self.title
