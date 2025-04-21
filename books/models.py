8e8a3cf4dcf2c9d5557dbec548bb414631bb7883
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-04-21 21:03:08 +0330

    fix: Remove non-essential fields from Book class

--- books/models.py
+++ books/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Book(models.Model):
+
+
+    def __str__(self):
+        return self.title
 
 
 class Role(models.Model):
@@ -7,21 +12,3 @@ class Role(models.Model):
 
 class BookAuthor(models.Model):
     role = models.OneToOneField(Role, on_delete=models.CASCADE)
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    summary = models.TextField(blank=True)
-    publisher = models.ForeignKey( null=True, blank=True, on_delete=models.SET_NULL, related_name='books')
-    published_date = models.DateField(null=True, blank=True)
-    cover = models.ImageField(null=True, blank=True)  # Consider ImageField for file-based images.
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    # Many-to-many relationship with authors is managed via a through model.
-    authors = models.ManyToManyField( through='BookAuthor', related_name='books')
-
-    def __str__(self):
-        return self.title
-
-
