263fb3e604d2854ea3b07c6881e856a83f7e042f
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-04-22 11:50:55 +0330

    feat: create CategorySerializer class

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Book, Publisher
+from .models import Book, Publisher,Category
 
 class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Publisher
         fields = ['id', 'name', 'address', 'website', 'logo', 'created_at', 'updated_at']
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
+
