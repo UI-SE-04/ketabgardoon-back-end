8265f24dc3ab1e2a8903626c60896de6f9965c25
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-23 14:59:38 +0330

    feat: Implemented Comment and UserLikeComment model

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-# from users.models import User
-# from books.models import Book
+from custom_users.models import CustomUser
+from books.models import Book
 
 class Comment(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     comment_text = models.TextField()
     reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,5 +15,5 @@ class Comment(models.Model):
 
 class UserCommentLike(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
