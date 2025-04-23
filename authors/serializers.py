86d617ed604ba32d79f58162d1eca13ba1b9738b
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-23 13:50:58 +0330

    fix: Missed 'death_date' field in authors/serializers.py added

--- authors/serializers.py
+++ authors/serializers.py
@@ -5,8 +5,8 @@ class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
         fields = [
-            'id', 'name', 'birth_date', 'nationality',
-            'bio', 'author_photo', 'call_info',
+            'id', 'name', 'birth_date', 'death_date',
+            'nationality', 'bio', 'author_photo', 'call_info',
             'created_at', 'updated_at',
         ]
         read_only_fields = ['created_at', 'updated_at']
