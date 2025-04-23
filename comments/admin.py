dbfb61ab2e5a38ead120aa9008cea57192f9e687
mahdimahdieh <mmahdieh90@gmail.com>
2025-04-23 15:00:03 +0330

    feat: Register comments in admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment, UserCommentLike
+
+admin.site.register(Comment)
+admin.site.register(UserCommentLike)
