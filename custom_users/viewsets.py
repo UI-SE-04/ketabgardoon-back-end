c7356a50cc58b8763eaa3a4d884ba8a0c5430c8a
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-06-01 09:17:32 +0330

    feat:add emailSubmissionView

--- custom_users/viewsets.py
+++ custom_users/viewsets.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, EmailSubmissionSerializer, EmailVerificationSerializer, CustomUserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
@@ -12,3 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [permissions.IsAuthenticatedOrReadOnly]
         return [permission() for permission in permission_classes]
+class EmailSubmissionView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = EmailSubmissionSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.create(serializer.validated_data)
+            return Response({"message": "email is sent", "email": user.email}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
