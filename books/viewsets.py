7d2f433b7c743f97eaea6b27c6f2faf3134387fd
mahdimahdieh <mmahdieh90@gmail.com>
2025-06-04 22:22:29 +0330

    feat: implemented the action during retrieve so it updates the view count

--- books/viewsets.py
+++ books/viewsets.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .models import Book, Publisher, Category, Store, Role, BookAuthor, BookStore
 from .serializers import BookSerializer, PublisherSerializer, CategorySerializer, StoreSerializer, RoleSerializer, \
     BookAuthorSerializer, BookISBN, BookISBNSerializer, BookStoreSerializer
+from .utils import has_viewed_today, mark_viewed_today
+from django.db.models import F
 
 
 class PublisherViewSet(viewsets.ModelViewSet):
@@ -68,12 +72,33 @@ class BookStoreViewSet(viewsets.ModelViewSet):
     filterset_fields = ['book__id', 'store__id']
     search_fields = ['store__name', 'url']
     ordering_fields = ['store__name']
+
+
 class BookViewSet(viewsets.ModelViewSet):
     """
     list, retrieve, create, update, partial_update, destroy
     """
-    queryset = Book.objects.all().order_by('published_date')
+    queryset = Book.objects.all().order_by('created_at')
     serializer_class = BookSerializer
     filterset_fields = ['publisher__id', 'published_date']
     search_fields = ['title', 'published_date','category__title']
     ordering_fields = ['published_date']
+    permission_classes = [permissions.AllowAny]
+
+    def retrieve(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs['pk'])
+        book_id = book.pk
+
+        if request.user.is_authenticated:
+            visitor_id = f'user:{request.user.id}'
+        else:
+            if not request.session.session_key:
+                request.session.save()
+            visitor_id = f'session:{request.session.session_key}'
+
+        if not has_viewed_today(visitor_id, book_id):
+            mark_viewed_today(visitor_id, book_id)
+            Book.objects.filter(pk=book_id).update(view_count=F('view_count') + 1)
+            book.refresh_from_db(fields=["view_count"])
+        serializer = self.get_serializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
