62e4927af37a7c617f69c32096d33f19422e537e
mahdimahdieh <mmahdieh90@gmail.com>
2025-06-08 21:46:44 +0330

    fix: basename and urls

--- ketabgardoon/urls.py
+++ ketabgardoon/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from api.v1.urls import router
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
