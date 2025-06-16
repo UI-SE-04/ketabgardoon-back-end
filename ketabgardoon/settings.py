f6afd20c7e5173fc0ac6071a016ce0fe4a975215
mahdimahdieh <mmahdieh90@gmail.com>
2025-06-16 16:04:25 +0330

    fix: save session and now drf can be logged-in

--- ketabgardoon/settings.py
+++ ketabgardoon/settings.py
@@ -160,7 +160,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+        'rest_framework.authentication.SessionAuthentication',
+    ],
 }
 
 
