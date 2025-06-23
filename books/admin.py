d45fe045aa683d252e3faae9e02cb8cc0505e7b7
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-06-23 23:41:45 +0330

    feat: edit admin panel of book app

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from books.models import Book , Publisher,BookAuthor,Role,BookISBN,Category,Store,BookStore
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Publisher)
-admin.site.register(BookAuthor)
-admin.site.register(Role)
-admin.site.register(BookISBN)
-admin.site.register(Category)
-admin.site.register(Store)
-admin.site.register(BookStore)
 
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publisher', 'published_date', 'view_count']
+    list_filter = ['publisher', 'categories', 'published_date']
+    filter_horizontal = ['categories']
+    search_fields = ['title', 'description']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
+
+admin.site.register(Publisher)
+admin.site.register(Store)
+admin.site.register(Role)
+admin.site.register(BookAuthor)
+admin.site.register(BookISBN)
+admin.site.register(BookStore)
