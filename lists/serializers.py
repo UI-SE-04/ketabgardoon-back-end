eb4c0eaff478d4700988826b79a09b057c6431e0
mahdimahdieh <mmahdieh90@gmail.com>
2025-06-26 20:17:45 +0330

    fix: sending authors into books' list

--- lists/serializers.py
+++ lists/serializers.py
@@ -41,10 +41,13 @@ class BookInListSerializer(serializers.ModelSerializer):
     book_title = serializers.CharField(source='book.title', read_only=True)
     book_cover = serializers.ImageField(source='book.cover', read_only=True)
     added_at = serializers.DateTimeField(read_only=True)
-
+    authors = serializers.SerializerMethodField()
     class Meta:
         model = BookList
-        fields = ['book_id', 'book_title', 'book_cover', 'added_at']
+        fields = ['book_id', 'book_title', 'book_cover', 'authors', 'added_at']
+
+    def get_authors(self, obj):
+        return [ author.name for author in obj.book.authors.all() ]
 
 
 
@@ -54,10 +57,10 @@ class BookListCreateSerializer(serializers.ModelSerializer):
     Only needs the book ID; the list is supplied by the view.
     """
     book_id = serializers.IntegerField(write_only=True)
-
+    authors = serializers.SerializerMethodField()
     class Meta:
         model = BookList
-        fields = ['book_id']
+        fields = ['book_id', 'authors']
 
     def validate_book_id(self, value):
         """
@@ -76,3 +79,6 @@ class BookListCreateSerializer(serializers.ModelSerializer):
             list=list_obj,
             book_id=validated_data['book_id']
         )
+
+    def get_authors(self, obj):
+        return [ author.name for author in obj.book.authors.all() ]
