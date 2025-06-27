8f343c31e27ffef3a6ef6620e7b2ee0364569a7d
SanaSobhani <126760883+SanaSobhani@users.noreply.github.com>
2025-06-27 13:12:37 +0330

    fix: edit serializers of search and add some fields

--- search/serializers.py
+++ search/serializers.py
@@ -5,22 +5,30 @@ from custom_users.models import CustomUser
 class BookSerializer(serializers.ModelSerializer):
     authors = serializers.StringRelatedField(many=True)
     categories = serializers.StringRelatedField(many=True)
+    ratings_count = serializers.IntegerField(read_only=True)
+    ratings_avg = serializers.FloatField(read_only=True)
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'authors', 'categories']
+        fields = ['id', 'title', 'authors', 'categories', 'ratings_count', 'ratings_avg']
+        read_only_fields = ['ratings_count', 'ratings_avg']
 
 class AuthorSerializer(serializers.ModelSerializer):
     nationality = serializers.CharField(source='nationality.name', allow_null=True)
+    total_ratings = serializers.IntegerField()
+    average_rating = serializers.FloatField()
+    author_photo = serializers.ImageField()
 
     class Meta:
         model = Author
-        fields = ['id', 'name', 'nationality']
+        fields = ['id', 'name', 'nationality', 'total_ratings', 'average_rating', 'author_photo']
+        read_only_fields = ['total_ratings', 'average_rating']
 
 class UserSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(allow_null=True)
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'first_name', 'last_name']
+        fields = ['id', 'username', 'image']
 
 class CategorySerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, source='book_set')
